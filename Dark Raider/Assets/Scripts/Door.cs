6d8ff6703e19c4c0c6c5826e847695b7ace2b9d2
Arturet <arturo@pep-project.org>
2017-02-22 15:18:14 +0100

    Added doors for moving between levels

---
+++ Dark Raider/Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public int levelID = -1;
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		if (!otherCollider.CompareTag ("Player")) {
+			return;
+		}
+
+		Application.LoadLevel (levelID);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
